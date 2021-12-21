8616496729076e601f46056fc27b3257f6685deb
James Keicher <jamesdkeicher@gmail.com>
2021-12-21 12:25:50 -0800

    Fetch all books function works as expected

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // import functions and grab DOM elements
+import { getBooks } from "./fetch-utils.js";
+
 
 // let state
 
@@ -6,6 +8,11 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const fetcher = async() => {
+    let allBooks = await getBooks();
+    console.log(allBooks);
+};
+fetcher();
 
 
 console.log("I'm a book shelf");
